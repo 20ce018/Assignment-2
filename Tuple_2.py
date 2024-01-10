0f2de38a5a62b972b4f4f4acdc64d71fe8168e7a
20ce018 <20ce018@charusat.edu.in>
2024-01-10 16:43:25 +0530

    Done

--- Tuple_2.py
+++ Tuple_2.py
@@ -1,3 +1,4 @@
-Tuple=1,12,21,17#Declate tuple without () is also tuple
+Tuple=1,12,43,17#Declate tuple without () is also tuple
 print(type(Tuple))
 print(Tuple[0])#Zeroth index values =1 will print
+print(Tuple[3])
