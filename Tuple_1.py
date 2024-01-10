ab4577d56a5e516f4fb231fe34fa7cf8dad7902d
Djkarmur <dipakkarmur45@gmail.com>
2024-01-10 16:16:14 +0530

    changed by dipak

--- Tuple_1.py
+++ Tuple_1.py
@@ -1,3 +1,4 @@
-Tuple=("Hello",1,True,90.90,9999)#Creating tuple using ()
+Tuple=("Hello world",1,90.90,9999)#Creating tuple using ()
 print(type(Tuple))#will print the type of Tuple
+print(Tuple)
 print(Tuple)
